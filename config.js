82ff923729367709dfdab19a029700110344c348
mquanggiu290-dotcom <mquanggiu290@gmail.com>
2025-12-07 12:27:31 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var myStreamConfig = {
     // 1. ลองใช้ลิงก์นี้ทดสอบก่อน (ถ้าเล่นได้ แสดงว่าโค้ดผ่าน)
-    "source": "https://aka-live1050.delivery.roku.com/7bdb9bb8-6f3b-42c3-9ea7-44ec93c849cd/t2-origin/out/v1/live.m3u8",
+    "source": "https://jmp2.uk/rok-0b4814b9a5e53029e2b9aba64ddc1df0.m3u8",
     
     // 2. ลิงก์เดิมของคุณ (jmp2) มันติด CORS จะเล่นไม่ได้บน GitHub Pages
     // "source": "https://jmp2.uk/plu-668c5d3bfd9eb2000882bb50.m3u8",
