ad0cc62a123b3895c83b952e596cebffa3454471
mquanggiu290-dotcom <mquanggiu290@gmail.com>
2025-12-07 12:15:44 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 var myStreamConfig = {
     // 1. ลองใช้ลิงก์นี้ทดสอบก่อน (ถ้าเล่นได้ แสดงว่าโค้ดผ่าน)
-    "source": "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+    "source": "https://stitcher-ipv4.pluto.tv/v1/stitch/embed/hls/channel/668c5d3bfd9eb2000882bb50livestitch/master.m3u8?deviceType=samsung-tvplus&deviceMake=samsung&deviceModel=samsung&deviceVersion=unknown&appVersion=unknown&deviceLat=0&deviceLon=0&deviceDNT=%7BTARGETOPT%7D&deviceId=%7BPSID%7D&advertisingId=%7BPSID%7D&us_privacy=1YNY&samsung_app_domain=%7BAPP_DOMAIN%7D&samsung_app_name=%7BAPP_NAME%7D&profileLimit=&profileFloor=&embedPartner=samsung-tvplus&profilesFromStream=true",
     
     // 2. ลิงก์เดิมของคุณ (jmp2) มันติด CORS จะเล่นไม่ได้บน GitHub Pages
     // "source": "https://jmp2.uk/plu-668c5d3bfd9eb2000882bb50.m3u8",
